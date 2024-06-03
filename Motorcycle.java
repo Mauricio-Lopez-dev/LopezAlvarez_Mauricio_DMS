27aebca81bf6c7c2842115c59258a70d61cb8229
Mauricio Lopez Alvarez <mauriciolopezalvarez@Mauricios-MBP.attlocal.net>
2024-06-03 10:02:55 -0400

    Adding classes

---
+++ Motorcycle.java
@@ -0,0 +1,12 @@
+/*
+ * Author: Mauricio Lopez Alvarez
+ * Date Written: June 3, 2024
+ * Course: CEN 3024C
+ * Class: Software Development I
+ * Purpose:...
+ */
+
+public class Motorcycle
+{
+
+} // end Motorcycle class
---
+++ UpdateValues.java
@@ -0,0 +1,10 @@
+/*
+ * Author: Mauricio Lopez Alvarez
+ * Date Written: June 3, 2024
+ * Course: CEN 3024C
+ * Class: Software Development I
+ * Purpose:...
+ */
+
+public class UpdateValues {
+}
